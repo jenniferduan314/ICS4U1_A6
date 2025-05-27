a809f2e4735e20838f7d3b57c44c2fe3c4ca179e
unknown <342532389@TDSB.ON.CA>
2025-05-27 15:04:32 -0400

    random footer ref changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,20 +4,20 @@ function Footer() {
     return (
         <div className="footer">
             <div className="footer-grid">
-                <a href="i">Account</a>
-                <a href="h">Media Notice</a>
+                <a href="/whoevenknows">Account</a>
+                <a href="/whyareyouclickingthis">Media Notice</a>
             </div>
             <div className="footer-grid">
-                <a href="g">Terms of Service</a>
-                <a href="f">Privacy Notice</a>
+                <a href="/goawayplz">Terms of Service</a>
+                <a href="/findsomethingelse">Privacy Notice</a>
             </div>
             <div className="footer-grid">
-                <a href="d">Redeem</a>
-                <a href="c">New Deals</a>
+                <a href="/dudegetalife">Redeem</a>
+                <a href="/cantdonothinghere">New Deals</a>
             </div>
             <div className="footer-grid">
-                <a href="b">FAQ</a>
-                <a href="a">© Meow Movies!</a>
+                <a href="/bestgotouchsomegrass">FAQ</a>
+                <a href="/awayyougo!">© Meow Movies!</a>
             </div>
         </div>
     )
